93d7ecc21730da15872e89fbb00aa464dd923c88
Tanner Unsworth <unzy98@hotmail.com>
2021-01-27 21:10:12 -0500

    *Main changed to reflect engine code

--- TheProgressEngine/Main.cpp
+++ TheProgressEngine/Main.cpp
@@ -1,20 +1,17 @@
-#include "Window.h"
+#include "ProgressEngine.h"
 
-Window* window;
+ProgressEngine* engine;
 
 int main(int argc, char* argv[]) {
-	Window* window = new Window();
-	if (!window->OnCreate("Progress Engine", 800, 600))
+	engine = new ProgressEngine();
+	if (!engine->OnCreate("Progress Engine", 800, 600))
 	{
-		delete window;
-		window = nullptr;
+		delete engine;
+		engine = nullptr;
 		return 0;
 	}
-	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	SDL_GL_SwapWindow(window->GetWindow());
-	SDL_Delay(5000);
-	delete window;
-	window = nullptr;
+	engine->Run();
+	delete engine;
+	engine = nullptr;
 	return 0;
 }
